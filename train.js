f303d2375b4b6dabea8cc81ff34cfbb7b454aa39
jakhongir789 <jakhongir789@gmail.com>
2025-06-05 21:54:48 +0900

    feat: train callback & async functions

--- train.js
+++ train.js
@@ -1,7 +1,3 @@
-// B-Task
-
-// A-Task
-
 console.log("Jack Ma Maslahatlari");
 const list = [
   "yaxshi talaba boling", // 0-20
@@ -12,6 +8,8 @@ const list = [
   "endi dam oling, foydasi yoq", // 60
 ];
 
+// CALLBACK Function:
+
 function maslahatBering(a, callback) {
   if (typeof a !== "number") callback("Insert a number", null);
   else if (a <= 20) callback(null, list[0]);
@@ -20,15 +18,65 @@ function maslahatBering(a, callback) {
   else if (a > 40 && a <= 50) callback(null, list[3]);
   else if (a > 50 && a <= 60) callback(null, list[4]);
   else {
-    setTimeout(function () {
+    setInterval(function () {
       callback(null, list[5]);
-    }, 5000);
+    }, 1000);
   }
 }
 
 console.log("passed here 0");
-maslahatBering(65, (err, data) => {
+maslahatBering(70, (err, data) => {
   if (err) console.log("ERROR:", err);
-  console.log("javob:", data);
+  else {
+    console.log(data);
+  }
 });
+
 console.log("passed here 1");
+
+// ASYNC FUNCTION:
+
+// // Definition qismda async function ishlatidi
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("Insert a number", null);
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     // return list[5];
+//     return new Promise((resolve, reject) => {
+//       setInterval(() => {
+//         resolve(list[5]);
+//       }, 1000);
+//     });
+
+//     // setTimeout(function () {
+//     //   return list[5];
+//     // }, 5000);
+//   }
+// }
+
+// // CALL via then & catch methodlari ishlatidi
+// console.log("passed here 0");
+// maslahatBering(65)
+//   .then((data) => {
+//     console.log("javob:", data);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR:", err);
+//   });
+
+// console.log("passed here 1");
+
+// //CALL via async&await. Call qismda async function ishlatildi. async & await methodlarini orqali qurib oldik
+// async function run() {
+//   let javob = await maslahatBering(65);
+//   console.log("javob:", javob);
+//     javob = await maslahatBering(75);
+//     console.log("javob:", javob);
+//     javob = await maslahatBering(40);
+//     console.log("javob:", javob);
+// }
+// run();
