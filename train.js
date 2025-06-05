4436aadad773aa1e6eabc30a0a6f5c70a47aed2a
jakhongir789 <jakhongir789@gmail.com>
2025-06-05 20:56:49 +0900

    feat: created callback function

--- train.js
+++ train.js
@@ -2,4 +2,33 @@
 
 // A-Task
 
-console.log("TRAIN AREA!");
+console.log("Jack Ma Maslahatlari");
+const list = [
+  "yaxshi talaba boling", // 0-20
+  "togri boshliq tanlang", //20-30
+  "ozingizga ishlashni boshlang", // 30-40
+  "siz kuchli bolgan narsalarni qiling", // 40-50
+  "yoshlarga investitsiya qiling", // 50-60
+  "endi dam oling, foydasi yoq", // 60
+];
+
+function maslahatBering(a, callback) {
+  if (typeof a !== "number") callback("Insert a number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    setTimeout(function () {
+      callback(null, list[5]);
+    }, 5000);
+  }
+}
+
+console.log("passed here 0");
+maslahatBering(65, (err, data) => {
+  if (err) console.log("ERROR:", err);
+  console.log("javob:", data);
+});
+console.log("passed here 1");
