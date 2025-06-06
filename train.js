50aa2798503674caf643b8094f01d68f77403a4f
jakhongir789 <jakhongir789@gmail.com>
2025-06-06 20:45:42 +0900

    fix: added MITASK Task A

--- train.js
+++ train.js
@@ -1,38 +1,55 @@
-console.log("Jack Ma Maslahatlari");
-const list = [
-  "yaxshi talaba boling", // 0-20
-  "togri boshliq tanlang", //20-30
-  "ozingizga ishlashni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq", // 60
-];
+// MITASK
+// TASK A:
 
-// CALLBACK Function:
+function countLetter(letter, soz) {
+  let count = 0;
 
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("Insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
-  else {
-    setInterval(function () {
-      callback(null, list[5]);
-    }, 1000);
+  for (let i = 0; i < soz.length; i++) {
+    if (soz[i] === letter) {
+      count++;
+    }
   }
+
+  return count;
 }
 
-console.log("passed here 0");
-maslahatBering(70, (err, data) => {
-  if (err) console.log("ERROR:", err);
-  else {
-    console.log(data);
-  }
-});
+console.log(countLetter("a", "Jack Ma Maslahatlari"));
 
-console.log("passed here 1");
+// console.log("Jack Ma Maslahatlari");
+// const list = [
+//   "yaxshi talaba boling", // 0-20
+//   "togri boshliq tanlang", //20-30
+//   "ozingizga ishlashni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq", // 60
+// ];
+
+// // CALLBACK Function:
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("Insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 1000);
+//   }
+// }
+
+// console.log("passed here 0");
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   else {
+//     console.log(data);
+//   }
+// });
+
+// console.log("passed here 1");
 
 // ASYNC FUNCTION:
 
