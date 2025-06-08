4e975670523e9b68ea7e6a862959ca6a8e9c0805
jakhongir789 <jakhongir789@gmail.com>
2025-06-08 21:10:27 +0900

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,60 +1,31 @@
-// Express => Nodejsda web serverlarni quradigan framework.
-
-console.log("Web Serverni boshladik");
-const express = require("express");
-const app = express(); // expressni App objectini yaratdik
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
+let db;
+const connectionString =
+  "mongodb+srv://jakhongir97m:justin97m!@cluster0.lkqjzho.mongodb.net/Reja";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connected successfully");
+      module.exports = client;
+
+      const app = require("./app");
+
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-
-// 1 KIRISH Code:
-
-app.use(express.static("public")); // browser expressga request qilayotganda public folderni clientlarga ochib beramiz
-app.use(express.json()); // kirib kelayotgan json formatdagi datani object holatiga o'girib beradi
-app.use(express.urlencoded({ extended: true })); //htmldagi formdan nimanidir post qilsak express qabul qivoladi
-
-// 2  Session Code:
-
-// 3  VIEW Code:  Backendda view yasash (view clientga yuboriladi, html orqali yasaladi)
-app.set("views", "views"); //view degan folderni korsatadi
-app.set("view engine", "ejs"); // viewni yasash ejs orqali
-
-// 4 ROUTING Code:
-
-////test
-// app.get("/hello", function (req, res) {
-//   res.end(`<h1 style="background: red">Hello World by Justin</h1>`);
-// });
-
-// app.get("/gift", function (req, res) {
-//   res.end(`<h1 style="background: red">Siz sovgalar bolimidasiz</h1>`);
-// });
-
-app.post("/create-item", (req, res) => {
-  console.log(req);
-  res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-  );
-});
+);
